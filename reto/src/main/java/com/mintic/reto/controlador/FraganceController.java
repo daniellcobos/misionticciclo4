43d43aa5d9bed6ccc69dbf15fa0c0e44686bfdff
Daniel <daniel.leocobos@gmail.com>
2021-12-01 10:08:52 -0500

    url change

--- reto/src/main/java/com/mintic/reto/controlador/FraganceController.java
+++ reto/src/main/java/com/mintic/reto/controlador/FraganceController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/supplements")
+@RequestMapping("/api/fragance")
 @CrossOrigin("*")
 public class FraganceController {
     @Autowired
